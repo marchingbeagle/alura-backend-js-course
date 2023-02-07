822abf5c36fbebf1f62e793ed75527a094b7fd1e
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-02-07 16:44:26 -0300

    regex link finder

--- Nodejs - first library/index.js
+++ Nodejs - first library/index.js
@@ -1,16 +1,29 @@
 import fs from "fs";
 import chalk from "chalk";
 
+function extractLinks(text) {
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s#.]*.[^\s]*)\)/gm;
+
+  // const capture = text.match(regex)
+  // const capture = regex.exec(text);
+  const capture = [...text.matchAll(regex)];
+  const result = capture.map((capture) => ({
+    [capture[1]]: [capture[2]],
+  }));
+  return result;
+}
+
 function treatError(error) {
   throw new Error(chalk.red(error.code, "Error detected"));
 }
 
-function getFile(filePath) {
-  const encoding = "utf-8";
-  fs.promises
-    .readFile(filePath, encoding)
-    .then((content) => console.log(chalk.green(content)))
-    .catch(treatError);
+async function getFile(filePath) {
+  try {
+    const encoding = "utf-8";
+    const content = await fs.promises.readFile(filePath, encoding);
+    console.log(extractLinks(content));
+  } catch (error) {
+    treatError(error);
+  }
 }
-
 getFile("./arquivos/texto.md");
