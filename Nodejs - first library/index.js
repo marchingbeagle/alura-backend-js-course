80734f48d5c9d6c836a56256ea8849826ee2f939
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-02-07 11:15:44 -0300

    error message test

--- Nodejs - first library/index.js
+++ Nodejs - first library/index.js
@@ -1,3 +1,18 @@
+import fs from "fs";
 import chalk from "chalk";
 
-console.log(chalk.blue("Hello World"));
+function treatError(error) {
+  throw new Error(chalk.red(error.code, "Error detected"));
+}
+
+function getFile(filePath) {
+  const encoding = "utf-8";
+  fs.readFile(filePath, encoding, (error, content) => {
+    if (error) {
+      treatError(error);
+    }
+    console.log(chalk.green(content));
+  });
+}
+
+getFile("./arquivos/texto.md");
