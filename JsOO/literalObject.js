9eac6d148bbb56dd3db0c1fccb4c05318d5958e6
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-02-10 18:58:09 -0300

    beginning of the OO js course added

---
+++ JsOO/literalObject.js
@@ -0,0 +1,10 @@
+const user = {
+  name: "Erik",
+  email: "erik@gmail.com",
+  birth: "2021/01/01",
+  role: "admin",
+  ativo: true,
+  showInfo: function () {
+    console.log("Nome:" + this.name + ", email:" + this.name);
+  },
+};
