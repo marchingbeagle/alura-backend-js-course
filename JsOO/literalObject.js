e01f45db3c40a39fa4ba5bc311a41e82925ebcc2
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-02-13 12:39:48 -0300

    prototype concept in practice

--- JsOO/literalObject.js
+++ JsOO/literalObject.js
@@ -1,17 +1,44 @@
 const user = {
+  name: "Pedro",
+  email: "pedro@gmail.com",
+  birth: "2021/01/01",
+  role: "student",
+  ativo: true,
+  showInfo: function () {
+    console.log(
+      `Name: ${this.name} | Email: ${this.email} | Role: ${this.role}`
+    );
+  },
+};
+
+const admin = {
   name: "Erik",
   email: "erik@gmail.com",
   birth: "2021/01/01",
   role: "admin",
-  ativo: true,
-  showInfo: function () {
-    console.log("Nome:" + this.name + ", email:" + this.email);
+  createCourse() {
+    console.log("Course created");
   },
 };
 
-const showUser = function () {
-  console.log("Nome:" + this.name + ", email:" + this.email);
-};
-const showEmail = showUser.bind(user);
+//quem herda // de quem herda
+Object.setPrototypeOf(admin, user);
+admin.createCourse();
+admin.showInfo();
+user.showInfo();
 
-showEmail();
+try {
+  user.createCourse();
+} catch (error) {
+  console.log("Sem permissao");
+}
+
+/*
+user.showInfo();
+const show = function () {
+  console.log(this.name);
+};
+show();
+
+const showName = show.bind(user);
+showName(); */
